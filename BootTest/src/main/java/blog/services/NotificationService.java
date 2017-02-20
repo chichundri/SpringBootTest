02b6637a0ac6b715e515f7040c1619ab107ccaed
GN00481175 <GN00481175@TechMahindra.com>
2017-02-20 16:09:43 +0530

    SpringBoot Posts application

---
+++ BootTest/src/main/java/blog/services/NotificationService.java
@@ -0,0 +1,5 @@
+package blog.services;
+public interface NotificationService {
+    void addInfoMessage(String msg);
+    void addErrorMessage(String msg);
+}
---
+++ BootTest/src/main/java/blog/services/UserService.java
@@ -0,0 +1,19 @@
+package blog.services;
+
+import java.util.List;
+
+import blog.models.User;
+
+public interface UserService {
+	boolean authenticate(String username, String password);
+
+	List<User> findAll();
+
+	User findById(Long id);
+
+	User create(User user);
+
+	User edit(User user);
+
+	void deleteById(Long id);
+}
